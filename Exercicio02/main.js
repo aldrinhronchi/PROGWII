3ada679cde4dc463216c94822ec009ce07643471
Terore <47401383+aldrinhronchi@users.noreply.github.com>
2020-11-23 20:52:32 -0300

    Exercicio 2 - feito

---
+++ Exercicio02/main.js
@@ -0,0 +1,9 @@
+var num = 5;
+var tabuada='';
+
+  for(var count=1; count<=10 ; count++)
+   tabuada += num+" x "+count+" = "+ num*count + " ";
+   console.log( tabuada);
+
+// Servidor web para google chrome
+// https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb
